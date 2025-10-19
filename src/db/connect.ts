6a234bc6f8e9f2f22956d8326a4511ac30970619
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-10-19 18:55:08 +0530

    Update MongoDB connection string in connect.ts to use 'bun_db' instead of 'blog'

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/blog");
+    await mongoose.connect("mongodb://localhost:27017/bun_db");
     console.log("✅MongoDB Connected Successfully!");
   } catch (error) {
     console.error("❌ MongoDB connection failed", error);
